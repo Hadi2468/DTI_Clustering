84f90029dbc4698e14d48a848e7bb815e9b572b8
Hadi Hosseini <hadi.hosseini@stjude.org>
2021-01-20 22:07:14 -0600

    update before ANOVA

--- R/HeatMap.R
+++ R/HeatMap.R
@@ -10,6 +10,9 @@ library(circlize)
 library (plyr)
 library(cluster)
 ##-------------------------------------------------------
+B_GIA       <- read.csv("../Data/Neurocognitive_Scores/Joins/cluster_base_GIA.csv")
+
+
 Heatmap(as.matrix(Data_SubjectAsRow_FeatureAsColumn), name="ComeUpWithAName",
         row_names_side="left",
         row_dend_side = "right",
@@ -21,11 +24,11 @@ Heatmap(as.matrix(Data_SubjectAsRow_FeatureAsColumn), name="ComeUpWithAName",
         clustering_method_rows = "complete",
         clustering_distance_columns = spdist1,
         clustering_method_columns = "complete",
-        show_column_names = VSFlag)+
+        show_column_names = VSFlag)
   Heatmap(AVectorOfClusterMembership,
           name = " ComeUpWithAName ",
           show_row_names = FALSE,
-          width = unit(5, "mm")) +
+          width = unit(5, "mm")) 
   Heatmap(AVectorOfDemographicOrNeurocognitiveVariable,
           name = " ComeUpWithAName ",
           show_row_names = FALSE,
