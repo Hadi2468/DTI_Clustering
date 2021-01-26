dacc0251f7afff169f552d4439dcb87b844492d1
Hadi Hosseini <hadi.hosseini@stjude.org>
2021-01-25 22:57:01 -0600

    Outer Join

--- R/K_Means.R
+++ R/K_Means.R
@@ -4,20 +4,44 @@ getwd()    # setwd("/Users/shossein/GitHub/DTI_Clustering/R")
 ##---------------------- K_Means --------------------------------------------------------------------------_____------------------------
 
 master_score <- read.csv("../../Data/data_random_1/Neurocognitive_Joins/master_score.csv")
+master_kmeans <- read.csv("../../Data/data_random_1/LF_Joins/master_kmeans.csv")
 LF_1500 <- read.csv("../../Data/data_random_1/LF/LF_1500_py.csv")
-LF <- LF_1500[ , -c(ID, class_e1500)]    # raw Latent Features
+LF <- LF_1500[-c(1:2)]    # raw Latent Features
 dim(LF)
 
 CL_2 <- kmeans(LF, 2)
 Kmeans_2_score <- CL_2$cluster
 length(Kmeans_2_score)
+Kmeans_2_score
 
 CL_4 <- kmeans(LF, 4)
 Kmeans_4_score <- CL_4$cluster
 length(Kmeans_4_score)
+Kmeans_4_score
 
-master_Kmeans <- data.frame(ID = LF_1500$ID, Kmeans_2 = Kmeans_2_score, Kmeans_4 = Kmeans_4_score)
-head(master_Kmeans)
+# mk <- master_kmeans
+# mk[2] <- Kmeans_2_score
+# mk[3] <- Kmeans_4_score
+# mk
+# mk[mk$Kmeans_4 == 1, ]$Kmeans_4 = 10
+# mk[mk$Kmeans_4 == 2, ]$Kmeans_4 = 30
+# mk[mk$Kmeans_4 == 3, ]$Kmeans_4 = 40
+# mk[mk$Kmeans_4 == 4, ]$Kmeans_4 = 20
+# mk
+# mk[mk$Kmeans_4 == 10, ]$Kmeans_4 = 1
+# mk[mk$Kmeans_4 == 20, ]$Kmeans_4 = 2
+# mk[mk$Kmeans_4 == 30, ]$Kmeans_4 = 3
+# mk[mk$Kmeans_4 == 40, ]$Kmeans_4 = 4
+# mk
+# mk[mk$Kmeans_4 != mk$Kmeans_Sparse_4, ]
+# mk[mk$Kmeans_2 != mk$Kmeans_Sparse_2, ]
+# write.csv(mk, "../../Data/data_random_1/LF_Joins/master_kmeans.csv", row.names = FALSE)
+
+master_kmeans <- read.csv("../../Data/data_random_1/LF_Joins/master_kmeans.csv")
+head(master_kmeans)
+master_kmeans
+master_kmeans[master_kmeans$Kmeans_4 != master_kmeans$Kmeans_Sparse_4, ]
+master_kmeans[master_kmeans$Kmeans_2 != master_kmeans$Kmeans_Sparse_2, ]
 
 ##---------------------- K_Means_Sparse ------------------------------------------------------------------------------------------------
 
@@ -28,7 +52,7 @@ print(K_2_bestw)         # K=2: bestw=54.88889
 
 km.out <- KMeansSparseCluster(LF, K = 2, wbounds = 54.88889)
 print(km.out)
-master_Kmeans["Kmeans_Sparse_2"] <- km.out[[1]]$Cs
+master_kmeans["Kmeans_Sparse_2"] <- km.out[[1]]$Cs
 
 km.perm <- KMeansSparseCluster.permute(LF, K = 4, wbounds = seq(58, 62, len = 15), nperms = 5)
 print(km.perm)
@@ -37,10 +61,11 @@ print(K_4_bestw)         # K=4: bestw=60.28571
 
 km.out <- KMeansSparseCluster(LF, K = 4, wbounds = 60.28571)
 print(km.out)
-master_Kmeans["Kmeans_Sparse_4"] <- km.out[[1]]$Cs
+master_kmeans["Kmeans_Sparse_4"] <- km.out[[1]]$Cs
 
 ##---------------------- Save data -----------------------------------------------------------------------------------------------------
 
-write.csv(master_Kmeans, "../../Data/data_random_1/LF_Joins/master_kmeans.csv", row.names = FALSE)
-master_data <- merge(x = master_Kmeans, y = master_score, by = "ID")
+master_data <- merge(x = master_kmeans, y = master_score, by = "ID")
+# colnames(master_data)[11] <- "Proc_Spd_diff"
+# colnames(master_data)[15] <- "Proc_Spd_diff"
 write.csv(master_data, "../../Data/data_random_1/LF_Joins/master_data.csv", row.names = FALSE)
--- R/Statistical_Analysis.R
+++ R/Statistical_Analysis.R
@@ -13,7 +13,7 @@ master_data$Kmeans_Sparse_2  <- factor(master_data$Kmeans_Sparse_2)
 master_data$Kmeans_Sparse_4  <- factor(master_data$Kmeans_Sparse_4)
 
 master_data$Kmeans_2 <- ordered(master_data$Kmeans_2,  levels = levels(master_data$Kmeans_2))
-master_data$Kmeans_2 <- ordered(master_data$Kmeans_4,  levels = levels(master_data$Kmeans_4))
+master_data$Kmeans_4 <- ordered(master_data$Kmeans_4,  levels = levels(master_data$Kmeans_4))
 master_data$Kmeans_Sparse_2 <- ordered(master_data$Kmeans_Sparse_2,  levels = levels(master_data$Kmeans_Sparse_2))
 master_data$Kmeans_Sparse_4 <- ordered(master_data$Kmeans_Sparse_4,  levels = levels(master_data$Kmeans_Sparse_4))
 
@@ -65,107 +65,98 @@ drsimonj_cols <- function(...) {
   if (is.null(cols))
     return (drsimonj_colors)
   drsimonj_colors[cols]}
-
-drsimonj_palettes <- list(
-  `cool`  = drsimonj_cols("blue", "red", "green", "yellow"),
-  `hot`   = drsimonj_cols("dark blue", "dark red", "dark green", "orange"))
-
+drsimonj_palettes <- list(`cool` = drsimonj_cols("blue", "red", "green", "yellow"), 
+                          `hot`  = drsimonj_cols("dark blue", "dark red", "dark green", "orange"))
 drsimonj_pal <- function(palette = "main", reverse = FALSE, ...) {
   pal <- drsimonj_palettes[[palette]]
   if (reverse) pal <- rev(pal)
   colorRampPalette(pal, ...)}
-
 scale_color_drsimonj <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
   pal <- drsimonj_pal(palette = palette, reverse = reverse)
-  if (discrete) {
-    discrete_scale("colour", paste0("drsimonj_", palette), palette = pal, ...)
-  } else {
-    scale_color_gradientn(colours = pal(256), ...)}}
-
+  if (discrete) {discrete_scale("colour", paste0("drsimonj_", palette), palette = pal, ...)}
+  else {scale_color_gradientn(colours = pal(256), ...)}}
 scale_fill_drsimonj <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
   pal <- drsimonj_pal(palette = palette, reverse = reverse)
-  if (discrete) {
-    discrete_scale("fill", paste0("drsimonj_", palette), palette = pal, ...)
-  } else {
-    scale_fill_gradientn(colours = pal(256), ...)}}
+  if (discrete) {discrete_scale("fill", paste0("drsimonj_", palette), palette = pal, ...)}
+  else {scale_fill_gradientn(colours = pal(256), ...)}}
 
-##---------------------- Box plots -----------------------------------------------------------------------------------------------
-
+##---------------------- Box plots for ANOVA & Kruskal–Wallis tests --------------------------------------------------------------------
+# Box-plot 1
 ggplot(master_data, aes(x = Kmeans_4, y = GIA_base, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.5961") + ylab("GIA") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 130))
-
+# Box-plot 2
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = GIA_base, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.9058") + ylab("GIA") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 130))
-
+# Box-plot 3
 ggplot(master_data, aes(x = Kmeans_4, y = Proc_Spd_base, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.9032") + ylab("Process Speed") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(30, 130))
-
+# Box-plot 4
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Proc_Spd_base, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.5335") + ylab("Process Speed") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(30, 130))
-
+# Box-plot 5
 ggplot(master_data, aes(x = Kmeans_4, y = Working_Mem_base, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.6187") + ylab("Working Memory") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 140))
-
+# Box-plot 6
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Working_Mem_base, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.8395") + ylab("Working Memory") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 140))
-
+# Box-plot 7
 ggplot(master_data, aes(x = Kmeans_4, y = Broad_Attn_base, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.8973") + ylab("Broad Attention") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 130))
-
+# Box-plot 8
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Broad_Attn_base, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.9984") + ylab("Broad Attention") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(50, 130))
-
+# Box-plot 9
 ggplot(master_data, aes(x = Kmeans_4, y = GIA_diff, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.1207") + ylab("GIA") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 30))
-
+# Box-plot 10
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = GIA_diff, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.3187") + ylab("GIA") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 30))
-
+# Box-plot 11
 ggplot(master_data, aes(x = Kmeans_4, y = Proc_Spd_diff, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.3373") + ylab("Process Speed") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 60))
-
+# Box-plot 12
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Proc_Spd_diff, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.1546") + ylab("Process Speed") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 60))
-
+# Box-plot 13
 ggplot(master_data, aes(x = Kmeans_4, y = Working_Mem_diff, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.3152") + ylab("Working Memory") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 30))
-
+# Box-plot 14
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Working_Mem_diff, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.7402") + ylab("Working Memory") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 30))
-
+# Box-plot 15
 ggplot(master_data, aes(x = Kmeans_4, y = Broad_Attn_diff, color = Kmeans_4, fill = Kmeans_4)) + 
   ggtitle("K-Means (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.7357") + ylab("Broad Attention") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + coord_cartesian(ylim = c(-50, 30))
-
+# Box-plot 16
 ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Broad_Attn_diff, color = Kmeans_Sparse_4, fill = Kmeans_Sparse_4)) + 
   ggtitle("K-Means Sparse (36 months - baseline)") + xlab("\n Kruskal–Wallis test,   p-value = 0.7983") + ylab("Broad Attention") + 
   geom_boxplot(size = 0.8, outlier.size = 3) + scale_fill_drsimonj(palette = "cool") + scale_color_drsimonj(palette = "hot") +
@@ -173,20 +164,35 @@ ggplot(master_data, aes(x = Kmeans_Sparse_4, y = Broad_Attn_diff, color = Kmeans
 
 ##---------------------- t test --------------------------------------------------------------------------------------------------------
 
-m36_2class <- m36base
-levels(m36_2class$Cluster_SS_e500)  <- c("1", "1", "2", "2")
-levels(m36_2class$Cluster_SS_e1500) <- c("1", "1", "2", "2")
-levels(m36_2class$Cluster_SS_e500)
+t.test(GIA_base        ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Proc_Spd_base    ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Working_Mem_base ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Broad_Attn_base  ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(GIA_base         ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Proc_Spd_base    ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Working_Mem_base ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Broad_Attn_base  ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
 
-t.test(GIA_SS         ~ Cluster_SS_e1500, data = m36_2class, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
-t.test(Proc_Spd_SS    ~ Cluster_SS_e1500, data = m36_2class, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
-t.test(Working_Mem_SS ~ Cluster_SS_e1500, data = m36_2class, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
-t.test(Broad_Attn_SS  ~ Cluster_SS_e1500, data = m36_2class, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(GIA_diff         ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Proc_Spd_diff    ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Working_Mem_diff ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Broad_Attn_diff  ~ Kmeans_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(GIA_diff         ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Proc_Spd_diff    ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Working_Mem_diff ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+t.test(Broad_Attn_diff  ~ Kmeans_Sparse_2, data = master_data, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
 
-t.test(cbind(GIA_SS, Proc_Spd_SS, Working_Mem_SS, Broad_Attn_SS) ~ Cluster_SS_e500, data=m36_2class, 
-       mu=0, alt="two.sided", conf=0.95, var.eq=F, paired=F)
+t.test(cbind(GIA_base, Proc_Spd_base, Working_Mem_base, Broad_Attn_base) ~ Kmeans_2, data = master_data, 
+       mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
 
+t.test(cbind(GIA_base, Proc_Spd_base, Working_Mem_base, Broad_Attn_base) ~ Kmeans_Sparse_2, data = master_data, 
+       mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
 
+t.test(cbind(GIA_diff, Proc_Spd_diff, Working_Mem_diff, Broad_Attn_diff) ~ Kmeans_2, data = master_data, 
+       mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+
+t.test(cbind(GIA_diff, Proc_Spd_diff, Working_Mem_diff, Broad_Attn_diff) ~ Kmeans_Sparse_2, data = master_data, 
+       mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
 
 ##---------------------- Wilcoxon rank sum test (Non-parametric alternative to t-test) -------------------------------------------------
 
